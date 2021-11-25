822609b19de39af389eba5fb288e13370b94a59c
EdisonPadilla <edison.padilla@epn.edu.ec>
2021-11-24 22:10:42 -0500

    mostrar/ocultar lista de opciones de cada opcion del menu

--- menu-functionality.js
+++ menu-functionality.js
@@ -1,17 +1,25 @@
-const productOption = document.querySelector("#product-option");
-const companyOption = document.querySelector("#company-option");
-const connectOption = document.querySelector("#connect-option");
-const productArrow = document.querySelector("#product-arrow");
-const companyArrow = document.querySelector("#company-arrow");
-const connectArrow = document.querySelector("#connect-arrow");
+const d = document;
+const productOption = d.querySelector(".product-option");
+const companyOption = d.querySelector(".company-option");
+const connectOption = d.querySelector(".connect-option");
+const productArrow = d.querySelector(".product-arrow");
+const companyArrow = d.querySelector(".company-arrow");
+const connectArrow = d.querySelector(".connect-arrow");
+const productOptionsList = d.querySelector(".product-options-list");
+const companyOptionsList = d.querySelector(".company-options-list");
+const connectOptionsList = d.querySelector(".connect-options-list");
+
 productOption.addEventListener("click", () => {
   productArrow.classList.toggle("rotate-arrow");
+  productOptionsList.classList.toggle("show-product");
 });
 
 companyOption.addEventListener("click", () => {
   companyArrow.classList.toggle("rotate-arrow");
+  companyOptionsList.classList.toggle("show-company");
 });
 
 connectOption.addEventListener("click", () => {
   connectArrow.classList.toggle("rotate-arrow");
+  connectOptionsList.classList.toggle("show-connect");
 });
