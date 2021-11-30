803e2dae2e109550a920a35d87170c9b83430b0d
Evanny <hbpgvanny@gmail.com>
2021-11-29 20:57:00 -0500

    menu en movil casi completo

--- menu-functionality.js
+++ menu-functionality.js
@@ -8,6 +8,9 @@ const connectArrow = d.querySelector(".connect-arrow");
 const productOptionsList = d.querySelector(".product-options-list");
 const companyOptionsList = d.querySelector(".company-options-list");
 const connectOptionsList = d.querySelector(".connect-options-list");
+const menuButton = d.querySelector(".ham-btn");
+const menu = d.querySelector(".nav-menus-box");
+const bars = d.querySelectorAll(".ham-btn span");
 
 productOption.addEventListener("click", () => {
   productArrow.classList.toggle("rotate-arrow");
@@ -22,4 +25,13 @@ companyOption.addEventListener("click", () => {
 connectOption.addEventListener("click", () => {
   connectArrow.classList.toggle("rotate-arrow");
   connectOptionsList.classList.toggle("show-list-options");
+});
+
+/*menu functionality on mobile device*/
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  bars.forEach((child) => {
+    child.classList.toggle("animation");
+  });
 });
