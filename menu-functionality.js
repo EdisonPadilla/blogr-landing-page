d451c3c543051a84937d3b9deb19474751ca9ca5
Evanny <hbpgvanny@gmail.com>
2021-12-07 22:52:08 -0500

    solucion completa

--- menu-functionality.js
+++ menu-functionality.js
@@ -1,37 +1,34 @@
 const d = document;
-const productOption = d.querySelector(".product-option");
-const companyOption = d.querySelector(".company-option");
-const connectOption = d.querySelector(".connect-option");
-const productArrow = d.querySelector(".product-arrow");
-const companyArrow = d.querySelector(".company-arrow");
-const connectArrow = d.querySelector(".connect-arrow");
-const productOptionsList = d.querySelector(".product-options-list");
-const companyOptionsList = d.querySelector(".company-options-list");
-const connectOptionsList = d.querySelector(".connect-options-list");
-const menuButton = d.querySelector(".ham-btn");
+const menu_button = d.querySelector(".ham-btn");
 const menu = d.querySelector(".nav-menus-box");
 const bars = d.querySelectorAll(".ham-btn span");
-
-productOption.addEventListener("click", () => {
-  productArrow.classList.toggle("rotate-arrow");
-  productOptionsList.classList.toggle("show-list-options");
-});
-
-companyOption.addEventListener("click", () => {
-  companyArrow.classList.toggle("rotate-arrow");
-  companyOptionsList.classList.toggle("show-list-options");
-});
-
-connectOption.addEventListener("click", () => {
-  connectArrow.classList.toggle("rotate-arrow");
-  connectOptionsList.classList.toggle("show-list-options");
-});
+const submenu_btn = d.querySelectorAll(".submenu-btn");
+const arrow_icon = d.querySelectorAll(".arrow");
 
 /*menu functionality on mobile device*/
 
-menuButton.addEventListener("click", () => {
+menu_button.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
   bars.forEach((child) => {
     child.classList.toggle("animation");
   });
 });
+
+for (let i = 0; i < submenu_btn.length; i++) {
+  submenu_btn[i].addEventListener("click", function () {
+    if (window.innerWidth <= 1024) {
+      const sub_menu = this.nextElementSibling;
+      const height = sub_menu.scrollHeight;
+      if (sub_menu.classList.contains("show-submenu")) {
+        sub_menu.classList.remove("show-submenu");
+        sub_menu.removeAttribute("style");
+      } else {
+        sub_menu.classList.add("show-submenu");
+        sub_menu.style.height = height + "px";
+        sub_menu.style.margin = "12px 0 0 0";
+        sub_menu.style.overflow = "visible";
+      }
+    }
+    arrow_icon[i].classList.toggle("rotate-arrow");
+  });
+}
